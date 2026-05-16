ac008adb7afcb1ab2944bad206a979beaa78e5af
Mihkel TKTK <mihkel.osa@tktk.ee>
2026-05-16 22:12:31 +0300

    esmane upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const images = ['1.png', '2.png', '3.png'];
+const targetImage = document.querySelector('.section.profile-photo');
+let i = 1;
+
+targetImage.addEventListener('click', () => {
+
+    targetImage.style.opacity = '0';
+
+    setTimeout(() => {
+
+        if (i === images.length) {
+            i = 0;
+        }
+
+        targetImage.src = 'images/' + images[i];
+        i++;
+
+        targetImage.style.opacity = '1';
+
+    }, 200);
+});
